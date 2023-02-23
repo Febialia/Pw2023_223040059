b4a3120be1c9a7724756cad09ccae7c43fb5a671
Febialia <119504728+Febialia@users.noreply.github.com>
2023-02-23 12:39:58 +0700

    Add files via upload

---
+++ tugas1/1b.php
@@ -0,0 +1,25 @@
+<?php
+$npm = '59';
+$kali5 = $npm * 9;
+$bagi2 = $npm / 2;
+$tambah75 = $npm + 75;
+$kurang20 = $npm - 20;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 2b</title>
+</head>
+<body>
+    <p><?php echo "Aku adalah angka <b>$npm</b>" ?></p>
+    <p><?php echo "jika aku dikali 5, maka aku sekarang menjadi <b>$kali5</b>" ?></p>
+    <p><?php echo "jika aku dibagi 2, maka aku sekarang menjadi <b>$bagi2</b>" ?></p>
+    <p><?php echo "jika aku ditambah 75, maka aku sekarang menjadi <b>$tambah75</b>" ?></p>
+    <p><?php echo "jika aku dikurang 20, maka aku sekarang menjadi <b>$kurang20</b>" ?></p>
+    
+</body>
+</html>
